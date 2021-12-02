a8cf5cb17e49046ab78e3285a840c2f47eca291f
valvazoripaulo <valvazori.paulo@gmail.com>
2021-12-02 20:10:02 -0300

    Added cities choice

---
+++ lib/weather_forecast/city.ex
@@ -0,0 +1,14 @@
+defmodule WeatherForecast.City do
+  defmodule City do
+    defstruct name: nil,
+              id: nil
+  end
+
+  def all, do: [
+    %City{name: "São Paulo, SP - BR", id: 3448439},
+    %City{name: "Rio de Janeiro, RJ - BR", id: 3451190},
+    %City{name: "Salvador, BA - BR", id: 3450554},
+    %City{name: "Caraguatatuba, SP - BR", id: 3467081},
+    %City{name: "Gramado, RS - BR", id: 3462284}
+  ]
+end
---
+++ lib/weather_forecast/cli/city_choice.ex
@@ -0,0 +1,39 @@
+defmodule WeatherForecast.CLI.CityChoice do
+  alias Mix.Shell.IO, as: Shell
+
+  def start do
+    Shell.cmd("clear")
+    Shell.info("Choose one of the cities :\n")
+
+    cities = WeatherForecast.City.all()
+    find_city_by_index = &Enum.at(cities, &1)
+
+    cities
+    |> display_options
+    |> generate_question
+    |> Shell.prompt
+    |> parse_answer
+    |> find_city_by_index.()
+    |> IO.inspect
+  end
+
+  defp display_options(options) do
+    options
+    |> Enum.with_index(1)
+    |> Enum.each(fn {option, index} ->
+      Shell.info("#{index} - #{option.name}")
+    end)
+
+    options
+  end
+
+  defp generate_question(options) do
+    opts = Enum.join(1..Enum.count(options), ",")
+    "\nWhich one? [#{opts}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+end
